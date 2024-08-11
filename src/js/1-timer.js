8844a5ad6b5b496fdd016ff005af5df556f5cdf1
Vidiborets Viktor <vidiboretsv@gmail.com>
2024-08-11 17:20:04 +0300

    import libraries into 1-timer.js

---
+++ src/js/1-timer.js
@@ -0,0 +1,5 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";  // імпорт стилів для flatpickr
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";  // імпорт стилів для iziToast
